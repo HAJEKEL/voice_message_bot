869714c90d13ef58e6f20bc3e278f39737458eb0
Henk <h.a.jekel@student.tudelft.nl>
2023-09-28 15:57:15 +0200

    updated the configuration files like the github repo of the teacher

--- frontend-react/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}",".index.html","./src/**/*.{js,tx,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
